5e9280705106dc86a9439629b23fed2b53d98ea2
Bates3 <113563344+BatesCubed@users.noreply.github.com>
2022-12-26 07:20:58 +0000

    Layout of HTML changed, css file moved. etc. Python working

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-from flask import Flask
+import random, string
+from flask import Flask, render_template
 
-app = Flask(__name__)
 
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
-def home():
-   return render_template('index.html')
-  
-if __name__ == '__main__':
-   app.run()
+def hello_world():
+  return render_template('index.html')
 
-
-app.run(host='0.0.0.0', port=81)
+app.run(host='0.0.0.0', port=8080)
